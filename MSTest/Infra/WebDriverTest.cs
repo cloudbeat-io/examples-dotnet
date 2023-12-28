18696081d14dc84274d9ce5499abdd24f110adb3
N. Dimer <ndimer@cloudbeat.io>
2023-12-28 12:27:44 +0100

    Add basic MSTest test project.

---
+++ MSTest/Infra/WebDriverTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CloudBeat.Kit.MSTest;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Events;
+
+namespace CbExamples.NUnit.Infra
+{
+    [TestClass]
+    public class WebDriverTest : CbTest
+    {
+        private IWebDriver _driver = null;
+
+        public EventFiringWebDriver Driver { get; private set; }
+
+        [TestInitialize]
+        public void SetUpWebDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            _driver = new ChromeDriver(options);
+            Driver = new EventFiringWebDriver(_driver);
+            CbMSTest.WrapWebDriver(Driver);
+        }
+
+        [TestCleanup]
+        public void DisposeWebDriver()
+        {
+            if (_driver != null)
+            {
+                try
+                {
+                    _driver.Close();
+                    _driver.Quit();
+                }
+                catch { }
+                _driver = null;
+                Driver = null;
+            }
+        }
+    }
+}
+
---
+++ MSTest/Tests/SauceDemo/LoginTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CbExamples.NUnit.Infra;
+using CbExamples.NUnit.Pages.SauceDemo;
+
+namespace CbExamples.NUnit.Tests.SauceDemo
+{
+    [TestClass]
+    public class LoginTests : WebDriverTest
+    {
+        [TestMethod("Standard user login behaviour")]
+        public void StandardUserTest()
+		{
+            var loginPage = new LoginPage(Driver);
+            loginPage.Open();
+            loginPage.AssertPageOpen();
+            loginPage.EnterUsername("standard_user");
+            loginPage.EnterPassword("secret_sauce");
+            loginPage.PressLoginButton();
+        }
+
+        [TestMethod("Locked out user login behaviour")]
+        public void LockedOutUserTest()
+        {
+
+        }
+
+        [TestMethod("Problem user login behaviour")]
+        public void ProblemUserTest()
+        {
+
+        }
+    }
+}
+
